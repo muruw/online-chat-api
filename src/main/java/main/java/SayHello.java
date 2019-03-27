0c11c9fbf7ee762f9c157c156980f53eeec37824
muruw <murumaem@gmail.com>
2019-03-27 13:14:19 +0200

    Added maven support for the project

---
+++ src/main/java/main/java/SayHello.java
@@ -0,0 +1,22 @@
+package main.java;
+
+public class SayHello {
+
+    public int hello(String greeting) {
+        String[] stringToList = greeting.split("");
+        stringToList[0] = stringToList[0].toUpperCase();
+
+        String newString = "";
+
+        for (String character : stringToList) {
+            newString += character;
+        }
+        System.out.println(newString);
+        System.out.println(greeting);
+        if (greeting.equals(newString)) {
+            return 1;
+        }
+        return -1;
+    }
+
+}
